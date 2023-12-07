e0504db38fdedb4178986cec64588e103b57e7ab
s0ulless <116080731+s0ulles@users.noreply.github.com>
2023-12-07 19:55:39 +0300

    react

---
+++ 22-useEffect-async-await/src/components/Posts.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import Post from './Post';
+
+const API_URL = 'http://jsonplaceholder.typicode.com/posts';
+
+function Posts() {
+    const [postas, setPosts] = useState([]);
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch(API_URL);
+                const postas = await res.json();
+                setPosts(postas);
+            } catch (error) {
+                setError(error.message);
+            }
+            setIsLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    if (error) {
+        return <h1>Error: {error}</h1>;
+    }
+
+    return (
+        <>
+            <h1>Posts</h1>
+            <hr />
+            {isLoading ? (
+                <h1>Loading...</h1>
+            ) : (
+                postas.map((post) => <Post key={post.id} {...post} />)
+            )}
+        </>
+    );
+}
+
+export default Posts;
